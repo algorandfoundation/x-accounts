162a3fa6a992837717750a477b1da572e173fa39
Bit D13 <bit@d13.co>
2026-04-29 13:54:51 +0200

    chore: portal: redirect to algorand.co/xchain

--- projects/portal/app/routes/index.tsx
+++ projects/portal/app/routes/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    throw redirect({ to: "/app" });
+    throw redirect({ href: "https://algorand.co/xchain" });
   },
 });
