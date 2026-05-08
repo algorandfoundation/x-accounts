6ed8f58fd9b8ddee5f4e0ce8bc6fbbf9d5d6f081
Sofia Nicoletti <sofia.nicoletti@algorand.foundation>
2026-05-08 12:51:03 +0200

    style: portal: apply format to new changes

--- projects/portal/app/routes/index.tsx
+++ projects/portal/app/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    if (import.meta.env.PROD) throw redirect({ href: "https://algorand.co/xchain" });
-    throw redirect({ to: "/app" });
+    if (import.meta.env.PROD) throw redirect({ href: "https://algorand.co/xchain" })
+    throw redirect({ to: "/app" })
   },
 })
